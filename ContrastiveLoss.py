6aca16bae273a0fe88c6dd08fd4fb24e5960d05c
Singlemu <yinhao_x@163.com>
2022-06-27 11:02:42 +0800

    Create ContrastiveLoss.py

---
+++ ContrastiveLoss.py
@@ -0,0 +1,10 @@
+class ContrastiveLoss(torch.nn.Module):
+    def __init__(self, margin=2.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+ 
+    def forward(self, output1, output2, label):
+        euclidean_distance = F.pairwise_distance(output1, output2)
+        loss_contrastive = torch.mean((label) * torch.pow(euclidean_distance, 2) +
+                                      (1-label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+        return loss_contrastive*0.5
